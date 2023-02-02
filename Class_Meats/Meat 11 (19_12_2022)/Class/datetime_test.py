2669b76d6d7fb424cfe0c039e6da476fe424c7df
Daniel Shchetinin <117108199+MrDaniel0511@users.noreply.github.com>
2023-02-02 03:05:46 +0200

    update

--- Class_Meats/Meat 11 (19_12_2022)/Class/datetime_test.py
+++ Class_Meats/Meat 11 (19_12_2022)/Class/datetime_test.py
@@ -1,19 +1,41 @@
-import datetime
-import random
+# import datetime
 
-def testing(dateNumberToName):
+# # timestamp = datetime.datetime.now()
+# # print(timestamp)
+# # print(timestamp.timestamp())
 
-    date_string = "2022-12-21, Mon, 20:10"
-    ret_val = dateNumberToName(date_string)
 
-    assert ret_val == "Thursday"
+# # dt2 = datetime.datetime.strptime("19-12-2022", "%d-%m-%Y")
+# # print(dt2)
+
+
+# # dt1 = datetime.datetime.now()
+# # print(dt1.strftime("Today is: %d.%m.%Y"))
+
+# # now = datetime.datetime.now()
+
+# # now_plus_1_day = now + datetime.timedelta(days=1)
+# # now_plus_1_day_correct = now_plus_1_day.strftime("\nDate: %d.%m.%Y\nTime: %H:%M:%S")
+# # print("Inserted:", now_plus_1_day)
+# # print("\nCorrected:", now_plus_1_day_correct)
+
+# def my_test(n: int):
+#     prod = 1
+#     for i in range(n):
+#         prod = prod * n
+        
+# if __name__ == "__main__":
+#     start = datetime.datetime.now()
+#     my_test(1000000)
+#     end = datetime.datetime.now()
+#     run_time = (end - start).total_seconds()
+#     print(run_time)
+
+
+
+class Car:
     
-
-def dateNumberToName(date_string):
-    t1 = datetime.datetime.strptime(date_string, "%Y-%m-%d, %a, %H:%M")
-    t1 += datetime.timedelta(days=3)
-    return t1.strftime("%A")
-
-# date_string = f"2022-12-19, Mon, 20:10"
-# week_name = dateNumberToName(date_string)
-# print(week_name)
+    def __init__(self, typecar, color):
+        
+        self.typecar = typecar
+        
--- GTA5RP/Log_Checker/Запуск.bat
+++
@@ -1,2 +0,0 @@
-python Programa.py
-pause
