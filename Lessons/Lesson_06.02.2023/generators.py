745796ecf6f7fc08c8a6313f83dc7d881a3f2a6d
Daniel Shchetinin <117108199+MrDaniel0511@users.noreply.github.com>
2023-06-17 19:55:24 +0300

    main

--- Lessons/Lesson_06.02.2023/generators.py
+++ Lessons/Lesson_06.02.2023/generators.py
@@ -1,7 +1,11 @@
 def batches(n: int, l: list):
-    pass
+    i = 0
+    while i < len(l):
 
+        yield l[i: i+n]
+        i += n
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for b in batches(3, my_list):
-    b
+if __name__ == '__main__':
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    for b in batches(3, my_list):
+        print(b)
