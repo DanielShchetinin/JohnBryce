43f5d931db9ef5ba04c579991797a26bfba7c3b3
Daniel Shchetinin <117108199+MrDaniel0511@users.noreply.github.com>
2023-06-17 20:32:49 +0300

    main

--- Lessons/Lesson_06.02.2023/generators.py
+++ Lessons/Lesson_06.02.2023/generators.py
@@ -1,21 +1,27 @@
-import datetime
-def batches(n: int, l: list):
-    i = 0
-    while i < len(l):
-
-        yield l[i: i+n]
-        i += n
+def my_gen():
+    n = 1
+    print("This is printed first")
+    yield n
+    print("After the first one!")
+    
+    n += 1
+    print("This is printed second time")
+    yield n
+    
+    n += 1
+    print("This is a last time!")
+    yield n
+    
+    
 
 if __name__ == '__main__':
-    start = datetime.datetime.now()
-    choose = input("\nДля начала напишите Start: ")
-    if choose == "Start":
-        my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-        for b in batches(3, 'sadfasdfsadfsadf'):
-            print(b)
-    start = datetime.datetime.now()
-    end = datetime.datetime.now()
-    date = end.strftime("%d.%m.%Y")
-    time = end.strftime("%H:%M")
-    run_time = (end - start).total_seconds()
-    print("\nProgram Run Time:", run_time, "Seconds\n")
+    a = my_gen()
+    
+    val1 = next(a)
+    print(val1)
+    
+    val1 = next(a)
+    print(val1)
+    
+    val1 = next(a)
+    print(val1)
