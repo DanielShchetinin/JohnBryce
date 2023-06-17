09ff99c0d5cb8c1288b3a9abcc9f26270ee3455b
Daniel Shchetinin <117108199+MrDaniel0511@users.noreply.github.com>
2023-06-17 20:43:36 +0300

    main

--- Lessons/Lesson_06.02.2023/generators.py
+++ Lessons/Lesson_06.02.2023/generators.py
@@ -1,26 +1,30 @@
-num = 10
+# num = 10
 
-def my_gen():
-    global num
-    num += 1
+# def my_gen():
+#     global num
+#     num += 1
     
-    n = 1
-    print("This is printed first")
-    yield n
-    print("After the first one!")
+#     n = 1
+#     print("This is printed first")
+#     yield n
+#     print("After the first one!")
     
-    n += 1
-    print("This is printed second time")
-    yield n
+#     n += 1
+#     print("This is printed second time")
+#     yield n
     
-    n += 1
-    print("This is a last time!")
-    yield n
+#     n += 1
+#     print("This is a last time!")
+#     yield n
     
     
-#check
-if __name__ == '__main__':
-    for i in my_gen():
-        print(i)
+# #check
+# if __name__ == '__main__':
+#     for i in my_gen():
+#         print(i)
     
-    print(num)
+#     print(num)
+    
+    
+def my_range(end):
+    
